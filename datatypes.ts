a873b0169101ae833520aa39553c244f90572502
PronetAI <pronetaicorp@gmail.com>
2022-07-12 12:17:15 -0700

    Update datatypes.ts

--- datatypes.ts
+++ datatypes.ts
@@ -5,4 +5,28 @@
 // undefined  |    tuple
 // void       |    enum
 // null       |    function
+// never      |    ----
+
+// number: alows all integers
+var variable : number = 100;
+
+// string: allows all strings
+var variable : string = "Hello World";
+
+// boolean: boolean is true or false
+var variable : boolean = true;
+
+// void
+var variable : void = undefined ;
+
+// null: nothing stored in variable
+var variable = null;
+
+// undefined: unknown 
+var variable = undefined;
+
 // never
+var variable : never = null;
+
+// any: any data types
+var variable : any;

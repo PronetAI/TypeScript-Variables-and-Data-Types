273811d1c81e8910854ac97f0f03eb1cc58d2f2a
PronetAI <pronetaicorp@gmail.com>
2022-07-12 12:11:13 -0700

    Create datatypes.ts

---
+++ datatypes.ts
@@ -0,0 +1,8 @@
+// Built-in data types | User Defined Data Types
+// number     |    array
+// string     |    class
+// boolean    |    interface
+// undefined  |    tuple
+// void       |    enum
+// null       |    function
+// never

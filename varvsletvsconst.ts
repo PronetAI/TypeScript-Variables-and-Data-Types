b19dc0af45b242fcd6fdbd255c53480d53b415c7
PronetAI <pronetaicorp@gmail.com>
2022-07-12 12:04:00 -0700

    Create varvsletvsconst.ts

---
+++ varvsletvsconst.ts
@@ -0,0 +1,6 @@
+const variable1 : string = "Hello World!"
+let variable2 : string = "Hello World!"
+var variable3 : string = "Hello World!"
+console.log(variable1);
+console.log(variable2);
+console.log(variable3);
